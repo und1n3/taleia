68477dd024877a8b961a7328b73180bd8b33804b
und1n3 <undineunda5@gmail.com>
2021-10-31 00:39:24 +0200

    adding endless scrolling view. In progress.

---
+++ app/src/main/java/com/example/taleia/RecyclerViewAdapter.kt
@@ -0,0 +1,71 @@
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.taleia.R
+
+class RecyclerViewAdapter(var mItemList: List<String?>?) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val VIEW_TYPE_ITEM = 0
+    private val VIEW_TYPE_LOADING = 1
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if (viewType == VIEW_TYPE_ITEM) {
+            val view: View =
+                LayoutInflater.from(parent.context).inflate(R.layout.item_row, parent, false)
+            ItemViewHolder(view)
+        } else {
+            val view: View =
+                LayoutInflater.from(parent.context).inflate(R.layout.item_loading, parent, false)
+            LoadingViewHolder(view)
+        }
+    }
+
+    override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {
+        if (viewHolder is ItemViewHolder) {
+            populateItemRows(viewHolder, position)
+        } else if (viewHolder is LoadingViewHolder) {
+            showLoadingView(viewHolder, position)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return if (mItemList == null) 0 else mItemList!!.size
+    }
+
+    /**
+     * The following method decides the type of ViewHolder to display in the RecyclerView
+     *
+     * @param position
+     * @return
+     */
+    override fun getItemViewType(position: Int): Int {
+        return if (mItemList!![position] == null) VIEW_TYPE_LOADING else VIEW_TYPE_ITEM
+    }
+
+    private inner class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var tvItem: TextView
+
+        init {
+            tvItem = itemView.findViewById(R.id.tvItem)
+        }
+    }
+
+    private inner class LoadingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var progressBar: ProgressBar
+
+        init {
+            progressBar = itemView.findViewById(R.id.progressBar)
+        }
+    }
+
+    private fun showLoadingView(viewHolder: LoadingViewHolder, position: Int) {
+        //ProgressBar would be displayed
+    }
+
+    private fun populateItemRows(viewHolder: ItemViewHolder, position: Int) {
+        val item = mItemList!![position]
+        viewHolder.tvItem.text = item
+    }
+}
--- app/src/main/java/com/example/taleia/SavedPromptsActivity.kt
+++ app/src/main/java/com/example/taleia/SavedPromptsActivity.kt
@@ -3,12 +3,85 @@ package com.example.taleia
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.NonNull
+
+import android.widget.ProgressBar
+
+import androidx.recyclerview.widget.RecyclerView
+
+import android.widget.TextView
+
+import android.view.LayoutInflater
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+
+import RecyclerViewAdapter
+import android.os.Handler
+
 
 class SavedPromptsActivity : AppCompatActivity() {
+
+    var recyclerView: RecyclerView? = null
+    var recyclerViewAdapter: RecyclerViewAdapter? = null
+    var rowsArrayList: ArrayList<String?> = ArrayList()
+    var isLoading = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_saved_prompts)
+        recyclerView = findViewById(R.id.recyclerView)
+        populateData()
+        initAdapter()
+        initScrollListener()
+    }
 
+    private fun populateData() {
+        var i = 0
+        while (i < 10) {
+            rowsArrayList.add("Item $i")
+            i++
+        }
+    }
 
+    private fun initAdapter() {
+        recyclerViewAdapter = RecyclerViewAdapter(rowsArrayList)
+        recyclerView!!.adapter = recyclerViewAdapter
+    }
+
+    private fun initScrollListener() {
+        recyclerView!!.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val linearLayoutManager = recyclerView.layoutManager as LinearLayoutManager?
+                if (!isLoading) {
+                    if (linearLayoutManager != null && linearLayoutManager.findLastCompletelyVisibleItemPosition() == rowsArrayList.size- 1) {
+                        //bottom of list!
+                        loadMore()
+                        isLoading = true
+                    }
+                }
+            }
+        })
+    }
+
+    private fun loadMore() {
+        rowsArrayList.add(null)
+        recyclerViewAdapter!!.notifyItemInserted(rowsArrayList.size - 1)
+        val handler = Handler()
+        handler.postDelayed(Runnable {
+            rowsArrayList.removeAt(rowsArrayList.size - 1)
+            val scrollPosition: Int = rowsArrayList.size
+            recyclerViewAdapter!!.notifyItemRemoved(scrollPosition)
+            var currentSize = scrollPosition
+            val nextLimit = currentSize + 25
+            while (currentSize - 1 < nextLimit) {
+                rowsArrayList.add("Item $currentSize")
+                currentSize++
+            }
+            recyclerViewAdapter!!.notifyDataSetChanged()
+            isLoading = false
+        }, 2000)
     }
 }
